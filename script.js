ecb4d1f17b501be6a5fa7ecd74be39f5016f6d95
illia <illia.tasminskyi@gmail.com>
2021-09-16 20:17:55 +0300

    [...new]

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
 "use strict";
 
-console.log('----------------------------------');
+let num = [1, 2, 3, 4, 5, 6];
+let num2 = [...num, 7];
 
-let arr = [6, 14, 3, 22, 5, 1];
-
-let mySort = function (a, b) { // сортировка для .sort
-   return a - b;
-};
-arr.sort(mySort);
-
-// arr.pop(6);
-arr.push('Hi');
-
-
-for (let velue of arr) {
-   console.log(velue);
-}
-
-console.log('----------------------------------');
-
-arr.forEach(function (item, i, arr) {
-   console.log(`${i}: ${item} --- ${arr.length}`);
-});
-
-console.log('----------------------------------');
+num[2] = 'new';
+console.log(num);
+console.log(num2);
