a3c352bb56902cbdc78651104c3ba941bd181e68
illia <illia.tasminskyi@gmail.com>
2021-09-16 18:14:05 +0300

    callback

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 "use strict";
 
-let text;
-
-function start() {
-   while(text == '' || text == null || isNaN(text)) {
-      text = +prompt('Сколько тебе лет?', '');
-   }
-   alert(text);
+function f1() {
+  console.log("1");
 }
 
-start();
+function f2(callback1, callback3) {
+  console.log("2");
+  callback1();
+  callback3();
+}
+
+function f3() {
+   console.log("3");
+}
+
+
+f2(f1, f3);
